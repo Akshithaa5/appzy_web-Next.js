36a118d1fef22a672bd856454a9d4d4be9fdae73
Akshitha Hebbar <111656423+Akshithaa5@users.noreply.github.com>
2024-08-02 19:36:17 +0530

    Update Video.tsx

--- app/components/Video.tsx
+++ app/components/Video.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+"use client";
+import React, { useRef } from 'react';
 import Image from 'next/image';
 
 const Video: React.FC = () => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const handlePlayClick = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
   return (
     <section id="video" className="video py-12 bg-gray-100 relative">
       <div className="container mx-auto text-center height-500px">
@@ -10,10 +19,11 @@ const Video: React.FC = () => {
         <div className="relative bg-white overflow-hidden flex justify-center items-center">
           <div className="w-full max-w-5xl">
             <video
+              ref={videoRef}
               title="Appzy Video Demo"
               className="w-full rounded-lg overflow-hidden"
               controls
-              poster="/assets/images/1 (3).png" // Optional poster image
+              poster="/assets/images/1 (3).png"
             >
               <source
                 src="/assets/videos/Playcodec%20-%20Animated%20Explainer%20Video%20Financial%20App.mp4"
@@ -22,15 +32,19 @@ const Video: React.FC = () => {
               Your browser does not support the video tag.
             </video>
           </div>
-          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer">
+          <div
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer"
+            onClick={handlePlayClick}
+            aria-label="Play video"
+          >
             <Image src="/assets/images/animated button.gif" alt="Play Button" width={160} height={150} />
           </div>
           <div className="absolute bottom-0 left-0 -mb-12 ml-40">
-              <Image src="/assets/images/bottom-left.png" alt="Bottom Left Image" width={150} height={100} />
-            </div>
-            <div className="absolute top-0 right-0 -mt-10 mr-40"> {/* Adjusted positioning */}
-              <Image src="/assets/images/top-right.png" alt="Top Right Image" width={150} height={100} />
-            </div>
+            <Image src="/assets/images/bottom-left.png" alt="Bottom Left Image" width={150} height={100} />
+          </div>
+          <div className="absolute top-0 right-0 -mt-10 mr-40">
+            <Image src="/assets/images/top-right.png" alt="Top Right Image" width={150} height={100} />
+          </div>
         </div>
       </div>
     </section>
